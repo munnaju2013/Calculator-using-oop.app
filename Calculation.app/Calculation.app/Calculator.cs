decf39056887eec8a6472e60a032037f16786b56
munnaju2013 <munnaju2013@gmail.com>
2014-09-27 12:55:09 +0600

    add file

---
+++ Calculation.app/Calculation.app/Calculator.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculation.app
+{
+    class Calculator
+    {
+       
+
+        public double Add(double firstNumber ,double secondNumber)
+        {
+            return firstNumber + secondNumber;
+        }
+
+        public double Subtract(double firstNumber , double secondNumber)
+        {
+            return Math.Abs( firstNumber - secondNumber);
+        }
+
+        public double Multiply(double firstNumber ,double secondNumber )
+        {
+            return firstNumber*secondNumber;
+        }
+
+        public double Divide(double firstNumber , double secondNumber)
+           
+        {
+            return firstNumber/secondNumber;
+        }
+
+        //public double GetResult()
+        //{
+            
+        //}
+    }
+}
